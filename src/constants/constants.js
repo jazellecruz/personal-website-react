08ec870d8d28227fa259fdef449339ada5a1b725
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-10 23:21:32 +0800

    change project desc

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,28 +1,29 @@
 
-const projectData = [{
-  title: "WeatherLabs - Web App",
-  desc: "This is the very first Node.js server that I've created from a Udemy course. I learned how to define routes to fetch weather data from OpenWeatherMap's API and send it to the client-side. WeatherLabs served as my first step to being consistent with my learning and to persist despite the hardships of learning on how to program.",
-  url: "https://weatherlabs.onrender.com",
-  gitHub: "https://github.com/jazellecruz/weather-labs-web-app",
-  img: "",
-  tools: ["Node.js", "Express", "OpenWeatherMap's API","EJS", "Render"]
+const projectData = [
+  {
+    title: "PseudoJSON - REST API",
+    desc: "A REST API that sends placeholder data to help you with developing your Frontend applications.",
+    url: "https://pseudojson.onrender.com",
+    gitHub: "https://github.com/jazellecruz/PseudoJSON-rest-api.git",
+    img: "",
+    tools: ["Node.js", "Express", "MongoDB", "Mongoose", "EJS"]
   },
   {
     title: "The Watcher - News Website",
-    desc: "After learning the fundamentals of React.js, I decided to up my game by challenging myself to learn the intermediate concepts and apply it by creating a news website. In this project, I learned how to route a react app, paginate the news results and automatically retrieve more by using the JS's Intersection Observer API.",
+    desc: "A news website that presents you the latest global headlines reported by the most trusted news sources.",
     url: "https://thewatcher.vercel.app",
     gitHub: "https://github.com/jazellecruz/the-watcher-news-website-react",
     img: "",
     tools: ["React.js", "NewsCatcher's API", "Material UI", "Axios", "Vercel"]
   },
   {
-    title: "PseudoJSON - REST API",
-    desc: "Now that I have learned how to fetch data with APIs, its time to learn how to SEND data through an API.",
-    url: "https://pseudojson.onrender.com",
-    gitHub: "https://github.com/jazellecruz/PseudoJSON-rest-api.git",
+    title: "WeatherLabs - Web App",
+    desc: "A mini weather web app that fetches the latest weather report of your city.",
+    url: "https://weatherlabs.onrender.com",
+    gitHub: "https://github.com/jazellecruz/weather-labs-web-app",
     img: "",
-    tools: ["Node.js", "Express", "MongoDB", "Mongoose", "EJS"]
-  }
+    tools: ["Node.js", "Express", "OpenWeatherMap's API","EJS", "Render"]
+    },
 ]
 
 export {projectData}
