45930dc774f6ec1d8cb1353adf6545e784a6d07d
Jazelle Cruz <jazellecruz22@gmail.com>
2023-03-12 23:50:58 +0800

    finished layout

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,33 +1,37 @@
 import "../styles/projectItem.css";
-import altImg from "../assets/folder-icon.png"
+import altImg from "../assets/app.png";
+import gh from "../assets/github.png";
+import link from "../assets/link.png"
 
 const ProjectItem = ({title, desc, url, gitHub, img, tools}) => {
 
   return (
     <div className="project-item">
-      <header>
-        <div className="project-top">
-          <img src={altImg} alt="project-img" className="project-img"/>
-          <div className="project-links-container">
-            <a href={gitHub} >
-              GitHub
-            </a>
-            <a href={url} >
-              Live Demo
-            </a>
-          </div>
-        </div>
-        <div className="project-info">
+      <div className="project-icon">
+        <img src={altImg} alt="project-img" className="project-img"/>
+      </div>
+      <div className="project-info">
+        <header>
           <p className="project-title bold-text">{title}</p>
           <p className="project-desc">{desc}</p>
-        </div>
-      </header>
+        </header>
         <footer>
-        <ul className="tech-list project">
-          {tools.map(tool => <li>{tool}</li>)}
-        </ul>
+          <ul className="tech-list project">
+            {tools.map(tool => <li>{tool}</li>)}
+          </ul>
+          <div className="project-links-container">
+            <a href={gitHub} >
+              <img src={gh} alt="gh-icon" />
+            </a>
+            {url &&  
+              <a href={url} >
+               <img src={link} alt="link-icon" /> 
+              </a>
+            }
+          </div>
         </footer>
       </div>
+    </div>
   )
 }
 
