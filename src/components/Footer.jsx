c8435ab24447bdad8eb8f9e80c7dabe534df9ea5
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-09 22:56:03 +0800

    initial file commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+
+  const style = {
+    padding: "8rem 0 28px",
+    textAlign: "center"
+  }
+  return (
+    <footer className="footer-section" style={style}>
+      <p className="footer-text bold-text">developed by Jazz Cruz</p>
+    </footer>
+  )
+}
+
+export default Footer
