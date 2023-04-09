33b4cc4f0e4b420c3d2b584c6065b19ec2f71616
Jazelle Cruz <jazellecruz22@gmail.com>
2023-04-09 23:02:30 +0800

    add/change data

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,36 +1,81 @@
 
 const projectData = [
   {
-    title: "PseudoJSON - REST API",
-    desc: "A REST API that sends placeholder data to help you with developing your Frontend applications.",
+    title: "PseudoJSON",
+    desc: "A REST API that sends JSON placeholder data to help you with developing your Frontend applications. Built with NodeJS, Express, and MongoDB.",
     url: "https://pseudojson.onrender.com",
     gitHub: "https://github.com/jazellecruz/PseudoJSON-rest-api.git",
-    img: "",
-    tools: ["Node.js", "Express", "MongoDB", "Mongoose", "EJS", "Render"]
+    img: "pseudojson.png",
+    tools: ["Node.js", "Express", "MongoDB", "Mongoose", "EJS", "Render"],
+    color: "#d9f8f3"
   },
   {
-    title: "The Watcher - News Website",
-    desc: "A news website that presents you the latest global headlines reported by the most trusted news sources.",
+    title: "The Watcher",
+    desc: "A news website that presents you the latest global headlines reported by the most trusted news sources. Built with ReactJS and Newscatchers' API.",
     url: "https://thewatcher.vercel.app",
     gitHub: "https://github.com/jazellecruz/the-watcher-news-website-react",
-    img: "",
-    tools: ["React.js", "NewsCatcher's API", "Material UI", "Axios", "Vercel"]
+    img: "the-watcher-logo.png",
+    tools: ["React.js", "NewsCatcher's API", "Material UI", "Axios", "Vercel"],
+    color: "#ffd6de"
   },
   {
-    title: "WeatherLabs - Web App",
-    desc: "A mini weather web app that fetches the latest weather report of your city.",
+    title: "Booked",
+    desc: "A personal book manager that I use to organize my books collection. Built with NodeJS, ReactJS, MySQL, and JWT.",
+    gitHub: "https://github.com/jazellecruz/weather-labs-web-app",
+    img: "booked.png",
+    tools: ["Node.js", "Express", "MySQL", "React.js", "Material UI", "JWT"],
+    color: "#e7e1fa"
+  },
+  {
+    title: "WeatherLabs",
+    desc: "A mini weather web app that fetches the latest weather report of your city. Built with NodeJS, Express, and OpenWeatherMap's API.",
     url: "https://weatherlabs.onrender.com",
     gitHub: "https://github.com/jazellecruz/weather-labs-web-app",
-    img: "",
-    tools: ["Node.js", "Express", "OpenWeatherMap's API","EJS", "Render"]
-    },
-    {
-    title: "Booked - Personal Book Manager",
-    desc: "A personal book manager that I use to organize my books collection.",
-    gitHub: "https://github.com/jazellecruz/weather-labs-web-app",
-    img: "",
-    tools: ["Node.js", "Express", "MySQL", "React.js", "Material UI", "JWT"]
-    },
+    img: "weatherlabs.png",
+    tools: ["Node.js", "Express", "OpenWeatherMap's API","EJS", "Render"],
+    color: "#fce4d7",
+  }
 ]
 
-export {projectData}
+const socials = {
+  github: "https://github.com/jazellecruz",
+  linkedIn: "https://www.linkedin.com/in/jazellecruz",
+  readCV: "https://read.cv/jazzcruz",
+}
+
+const skills = [
+  {
+    tool: "JavaScript",
+    icon: "javascript.svg",
+  },
+  {
+    tool: "NodeJS",
+    icon: "nodejs-icon.svg",
+  },
+  {
+    tool: "ReactJS",
+    icon: "react-2.svg",
+  },
+  {
+    tool: "MySQL",
+    icon: "mysql-6.svg",
+  },
+  {
+    tool: "MongoDB",
+    icon: "mongodb-icon-1.svg",
+  },
+  {
+    tool: "Postman",
+    icon: "postman.svg",
+  },
+  {
+    tool: "Git",
+    icon: "git-logo.svg",
+  },
+  {
+    tool: "GitHub",
+    icon: "github-logo.svg",
+  },
+]
+
+export {projectData, socials, skills}
